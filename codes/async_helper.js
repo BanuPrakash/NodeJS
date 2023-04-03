d9c96f83281a9bb78d05a948c0a8a59cc2e8c326
Banuprakash <banuprakashc@yahoo.co.in>
2023-04-03 15:49:27 +0530

    day  1

---
+++ codes/async_helper.js
@@ -0,0 +1,26 @@
+const https = require('https');
+
+const start = Date.now();
+
+function doRequest() {
+    https.request("https://jsonplaceholder.typicode.com/users", response => {
+        response.on("data", (chunk) => {
+          //  console.log(chunk.toString())
+        }),
+        response.on("end", () => {
+            console.log(Date.now() - start);
+        })
+    }).end();
+}
+// Thread pool is not used for any network based operations [socket]
+// libuv uses Socket connection
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
